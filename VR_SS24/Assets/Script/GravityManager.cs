2af90755d2b4fa3a51e52e698e5318d78b26d1a7
Erwin <erwin_oudomvylay@hotmail.de>
2024-06-04 19:47:14 +0200

    gravity value for different planets

---
+++ VR_SS24/Assets/Script/GravityManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+public enum gravitationalForce { Space, Earth, Moon, Jupiter }
+public class GravityManager : MonoBehaviour
+{
+    public gravitationalForce gravitationalForce;
+    void Start()
+    {
+        switch (gravitationalForce)
+        {
+            case gravitationalForce.Space:
+                Physics.gravity = new Vector3(0, 0, 0);
+                break;
+            case gravitationalForce.Earth:
+                Physics.gravity = new Vector3(0, -9.81f, 0);
+                break;
+            case gravitationalForce.Moon:
+                Physics.gravity = new Vector3(0, -1.622f, 0);
+                break;
+            case gravitationalForce.Jupiter:
+                Physics.gravity = new Vector3(0, -24.79f, 0);
+                break;
+        }
+    }
+}
--- VR_SS24/Assets/Script/Spawn.cs
+++ VR_SS24/Assets/Script/Spawn.cs
@@ -1,23 +1,23 @@
 using UnityEngine;
-using UnityEngine.XR.LegacyInputHelpers;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SpawnBall : MonoBehaviour
 {
     [SerializeField] Transform spawnPoint;
-    // Start is called before the first frame update
+    private Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
 
     }
+    //Spawn ball when pressing B || Y
     public void Spawn()
     {
-            transform.position = spawnPoint.transform.position;
-            transform.rotation = spawnPoint.transform.rotation;
+        transform.position = spawnPoint.transform.position;
+        transform.rotation = spawnPoint.transform.rotation;
+        rb.velocity = Vector3.zero;
     }
 }
