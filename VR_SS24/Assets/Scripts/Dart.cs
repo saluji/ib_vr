9fc1649bb477bc4512779038cdbcc9bc9c625b38
Erwin <erwin_oudomvylay@hotmail.de>
2024-06-22 21:59:37 +0200

    respawn button

---
+++ VR_SS24/Assets/Scripts/Dart.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart : MonoBehaviour
+{
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Target"))
+        {
+            rb.isKinematic = true;
+        }
+    }
+    public void ResetPosition()
+    {
+        rb.isKinematic = false;
+    }
+}
---
+++ VR_SS24/Assets/Scripts/SpawnBasketball.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Drawing;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SpawnBasketball : MonoBehaviour
+{
+    [SerializeField] GameObject[] spawnObject;
+    [SerializeField] Transform[] spawnPoint;
+    //private Rigidbody[] rb;
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+    public void SpawnObject()
+    {
+        for (int i = 0; i < spawnObject.Length - 1; i++)
+        {
+            spawnObject[i].transform.position = spawnPoint[i].transform.position;
+            //transform.position = spawnPoint.transform.position;
+            //transform.rotation = spawnPoint.transform.rotation;
+            //rb[i].velocity = Vector3.zero;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("Button activated");
+        }
+    }
+}
