54872d6208cab7c67b1901c7886d12d5855b7e97
Erwin <erwin_oudomvylay@hotmail.de>
2024-06-24 21:10:13 +0200

    scene management
    load prefabs depending on scene

--- VR_SS24/Assets/Scripts/SpawnManager.cs
+++ VR_SS24/Assets/Scripts/SpawnManager.cs
@@ -11,11 +11,8 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] GameObject dartButton;
     [SerializeField] GameObject canButton;
     [SerializeField] Transform[] spawnBallPosition;
-    [SerializeField] GameObject[] spawnDartObject;
     [SerializeField] Transform[] spawnDartPosition;
-    [SerializeField] GameObject[] spawnCanObject;
     [SerializeField] Transform[] spawnCanPosition;
-    [SerializeField] GameObject spawnTennisballObject;
     [SerializeField] Transform spawnTennisballPosition;
     private int basketballAmount = 5;
     private int dartAmount = 3;
@@ -23,52 +20,50 @@ public class SpawnManager : MonoBehaviour
     private Rigidbody dartRb;
     void Awake()
     {
+        /*if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SpawnDart();
+            SpawnCan();
+            SpawnTennisball();
+            dartRb = dart.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            SpawnBasketball();
+        }*/
+        SpawnDart();
+        SpawnCan();
+        SpawnTennisball();
+        dartRb = dart.GetComponent<Rigidbody>();
+        SpawnBasketball();
+    }
+    public void SpawnBasketball()
+    {
         for (int i = 0; i < basketballAmount; i++)
         {
             Instantiate(basketball, spawnBallPosition[i].transform.position, spawnBallPosition[i].transform.rotation);
         }
+    }
+    public void SpawnDart()
+    {
         for (int i = 0; i < dartAmount; i++)
         {
             Instantiate(dart, spawnDartPosition[i].transform.position, spawnDartPosition[i].transform.rotation);
         }
+    }
+    public void SpawnCan()
+    {
         for (int i = 0; i < canAmount; i++)
         {
             Instantiate(can, spawnCanPosition[i].transform.position, spawnCanPosition[i].transform.rotation);
         }
-        dartRb = dart.GetComponent<Rigidbody>();
+    }
+    public void SpawnTennisball()
+    {
         Instantiate(tennisball, spawnTennisballPosition.transform.position, spawnTennisballPosition.transform.rotation);
     }
-    public void SpawnBasketball()
-    {
-        /*for (int i = 0; i < basketballAmount - 1; i++)
-        {
-            basketballAmount[i].transform.position = spawnBallPosition[i].transform.position;
-            basketballAmount[i].transform.rotation = spawnBallPosition[i].transform.rotation;
-            //ballRb[i].velocity = Vector3.zero;
-        }*/
-    }
-    public void SpawnDart()
-    {
-        /*for (int i = 0; i < dartAmount; i++)
-        {
-            spawnDartObject[i].transform.position = spawnDartPosition[i].transform.position;
-            spawnDartObject[i].transform.rotation = spawnDartPosition[i].transform.rotation;
-        }*/
-    }
-    public void SpawnCan()
-    {
-        //tennisballRb.velocity = Vector3.zero;
 
-        /*spawnTennisballObject.transform.position = spawnTennisballPosition.transform.position;
-        spawnTennisballObject.transform.rotation = spawnTennisballPosition.transform.rotation;
-
-        for (int i = 0; i < canAmount; i++)
-        {
-            spawnCanObject[i].transform.position = spawnCanPosition[i].transform.position;
-            spawnCanObject[i].transform.rotation = spawnCanPosition[i].transform.rotation;
-        }*/
-    }
-    private void OnCollisionEnter(Collision other)
+    public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Target"))
         {
@@ -76,7 +71,11 @@ public class SpawnManager : MonoBehaviour
         }
         if (other.gameObject.CompareTag("Delete"))
         {
-            Destroy(gameObject);
+            DestroySpawnedObject();
         }
+    }
+    public void DestroySpawnedObject()
+    {
+        Destroy(gameObject);
     }
 }
