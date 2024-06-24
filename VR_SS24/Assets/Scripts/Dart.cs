5cadbca645f02548da7c8cef88dc25291e577351
Erwin <erwin_oudomvylay@hotmail.de>
2024-06-24 23:12:21 +0200

    prefab physics change

--- VR_SS24/Assets/Scripts/Dart.cs
+++ VR_SS24/Assets/Scripts/Dart.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dart : MonoBehaviour
-{private Rigidbody rb;
+{
+    private Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
