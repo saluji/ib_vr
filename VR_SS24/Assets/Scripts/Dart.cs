71899779c81dfa7ec5cb1ea69d37aea9cda6697b
Erwin <erwin_oudomvylay@hotmail.de>
2024-06-24 22:47:11 +0200

    dart script

--- VR_SS24/Assets/Scripts/Dart.cs
+++ VR_SS24/Assets/Scripts/Dart.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dart : MonoBehaviour
-{
-    public Rigidbody rb;
+{private Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
