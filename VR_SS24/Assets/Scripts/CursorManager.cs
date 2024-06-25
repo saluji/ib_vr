ef0812578fcda5e98b0aedbdfb7b3d27e488cd37
Erwin <erwin_oudomvylay@hotmail.de>
2024-06-25 06:19:28 +0200

    UI click

--- VR_SS24/Assets/Scripts/CursorManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class CursorManager : MonoBehaviour
-{
-    private void OnApplicationFocus(bool focus)
-    {
-        Cursor.lockState = focus ? CursorLockMode.Locked : CursorLockMode.None;
-    }
-}
